7f5936b0ee712a630f9c599ba6f81cbac8e42c7e
techyyy <ukrainianbtch@gmail.com>
2021-11-07 14:08:30 +0200

    v 1.0

--- src/main/java/util/DateUtil.java
+++ src/main/java/util/DateUtil.java
@@ -7,18 +7,12 @@ public final class DateUtil {
 
     private static final SimpleDateFormat ISO_8601_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 
-    private DateUtil() {
-    }
-    public static synchronized Date fromString(String date) {
+    public static Date fromString(String date) {
         try {
             return ISO_8601_DATE_FORMAT.parse(date);
         } catch (Exception e) {
             System.err.println("Exception with parsing Date");
             throw new RuntimeException(e);
         }
-    }
-
-    public static synchronized String toString(Date startDate) {
-        return ISO_8601_DATE_FORMAT.format(startDate);
     }
 }
